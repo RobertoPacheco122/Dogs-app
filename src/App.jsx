b45cf5cca047f292bb672abfb172337414c8c8f0
Roberto <Robertoaraujo230@hotmail.com>
2023-06-08 15:26:42 -0300

    Criando estrutura inicial do componente App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./Components/Home";
+import Header from "./Components/Header";
+import "./assets/css/reset.css";
+import "./assets/css/utility.css";
+import "./assets/css/icons.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="app">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
